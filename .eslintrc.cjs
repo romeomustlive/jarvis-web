412da38983b3f7301fa80af7aab87e8637a3fd0e
Sergei Leushin <sergei.leushin@activeplatform.com>
2022-10-08 02:29:20 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ]
+}
