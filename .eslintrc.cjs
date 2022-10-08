5b7478e934f5b69d7f6d173147325c1d8187e575
Sergei Leushin <sergei.leushin@activeplatform.com>
2022-10-08 15:18:02 +0300

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,31 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
   ],
+  plugins: ['import', 'eslint-plugin-import-helpers'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/^~//', '/^@//'],
+        alphabetize: { order: 'asc', ignoreCase: false },
+      },
+    ],
+  },
 }
